e2c821a7c5cb938fd950c6dbb3fc20dbf9491c22
liufan <785454405@qq.com>
2023-01-29 11:26:16 +0800

    deploy

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -16,7 +16,7 @@ const count = ref(0)
       <code>components/HelloWorld.vue</code> to test HMR
     </p>
   </div>
-  <div>my name is lileilei2</div>
+  <div>my name is lileilei234</div>
   <p>
     Check out
     <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
