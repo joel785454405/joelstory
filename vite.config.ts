bbab338936d81f97c8455feb3d12a21a7a5c3f67
liufan <785454405@qq.com>
2023-01-29 11:39:16 +0800

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,26 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ArcoResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode, ssrBuild }) => {
   console.log(command, mode, ssrBuild, 6);
 
   return {
-    plugins: [vue()],
+    plugins: [
+      vue(),
+      AutoImport({
+        resolvers: [ArcoResolver()],
+      }),
+      Components({
+        resolvers: [
+          ArcoResolver({
+            sideEffect: true
+          })
+        ]
+      })],
     base: mode === 'development' ? '/' : './'
   }
 })
